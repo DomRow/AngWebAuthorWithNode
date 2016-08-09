e66422314e2e25da14d035f33671e7d8782c4dd7
DomRown <domrowaned@gmail.com>
2016-08-09 12:15:22 +0100

    added .bat file for speedy access

---
+++ mycmd.bat
@@ -0,0 +1,12 @@
+@echo off
+DOSKEY 1=cd..
+DOSKEY 2=cd C:\xampp\htdocs\~AA1\BasicAng
+DOSKEY 3=nodemon server.js
+DOSKEY 4=cd C:\mongo\bin
+DOSKEY 5=mongod --dbpath C:\Users\user\Desktop\data
+DOSKEY 6=mongo.exe
+DOSKEY 7=rs 
+DOSKEY 8=^C
+DOSKEY 9=exit
+
+
